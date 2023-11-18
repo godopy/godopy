c9515c34737b08892e3628cb29aa3e166ee2e541
Ivan Hilaire <ivhilaire@gmail.com>
2023-11-18 14:10:59 +0400

    Update demo project

--- project/test.gd
+++ project/test.gd
@@ -1,19 +1,19 @@
 extends SceneTree
 
 func _init():
-    Python.run_simple_string(
-        "import sys\n" +
-        "for p in sys.path:\n" +
-        "    print(p)\n" +
-        "import numpy as np\n" +
-        "a1D = np.array([1, 2, 3, 4])\n" +
-        "print(a1D)\n" +
-        "raise Exception('Test exc')"
-    )
+	Python.run_simple_string(
+		"import sys\n" +
+		"for p in sys.path:\n" +
+		"    print(p)\n" +
+		"import numpy as np\n" +
+		"a1D = np.array([1, 2, 3, 4])\n" +
+		"print(a1D)\n" +
+		"raise Exception('Test exc')"
+	)
 
-    Python.run_simple_string("print('Hello, world!')")
+	Python.run_simple_string("print('Hello, world!')")
 
-    quit()
+	quit()
 
-    # Python.initialize()
-    # quit(Python.run_main())
+	# Python.initialize()
+	# quit(Python.run_main())
