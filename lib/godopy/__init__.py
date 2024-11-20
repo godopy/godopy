c4dfd4fec0272bc4b1a2a0e0f988109ee9750fcb
Ivan Hilaire <ivhilaire@gmail.com>
2024-11-20 14:19:15 +0400

    Add godopy.constants module

---
+++ lib/godopy/__init__.py
@@ -0,0 +1,7 @@
+from .constants import __all__ as all_constants
+from .constants import *
+from .project import Project
+from .scene import Scene
+
+__all__ =  ['Project', 'Scene']
+__all__ += all_constants
